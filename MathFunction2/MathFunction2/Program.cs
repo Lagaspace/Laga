a0683e1965d2e19887942caa56173c802e3ef249
cidelab <cidelab@gmail.com>
2019-10-13 14:56:31 +0100

    update examples

---
+++ MathFunction2/MathFunction2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Laga.GeneticAlgorithm;
+using Laga.Numbers;
+
+namespace MathFunction2
+{
+    class Program
+    {
+        private static int popSize = 100;
+        private static Population<double> pop = new Population<double>(popSize);
+        static void Main(string[] args)
+        {
+            float mutRate = 0.025f;
+            double best;
+            int c = 1;
+            double target = 0.93749;
+
+            for (int i = 0; i < popSize; i++)
+                pop.Add(new Chromosome<double>(Rand.RandomNumbers(2, 0.0, 1.0)));
+
+            do
+            {
+                Evaluation();
+                best = pop.Higher().Fitness;
+
+                PrintData(pop.Higher(), c, mutRate, pop.FitnessAverage(), target);
+
+
+                Population<double> matPool = NaturalSelection<double>.RouletteWheelNonPolinomicMin(pop, 30);
+                pop = Crossover<double>.SinglePointCrossover(matPool, popSize, 1);
+                pop = Mutation.Number(pop, mutRate, 0.0, 1.00);
+
+                c++;
+            } while (best <= target);
+
+            Console.ReadLine();
+        }
+
+        private static void Evaluation()
+        {
+            double x, y;
+            foreach (Chromosome<double> cr in pop)
+            {
+                x = cr.GetDNA(0);
+                y = cr.GetDNA(1);
+                cr.Fitness = 15 * x * y * (1 - x) * (1 - y) * Math.Sin(Math.PI * x) * Math.Sin(Math.PI * y);
+            }
+        }
+
+        private static void PrintData(Chromosome<double> best, int genCount, float mut, double ave, double th)
+        {
+            Console.SetCursorPosition(0, 1);
+            Console.Write(" Best individual: ");
+            Console.WriteLine($" Param X: {best.GetDNA(0).ToString()}, Param Y: {best.GetDNA(1).ToString()}");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ////////////// GA data");
+            Console.WriteLine(" Generation: " + genCount.ToString());
+            Console.WriteLine(" Target: " + th.ToString());
+            Console.WriteLine(" Highest fitness: " + best.Fitness.ToString());
+            Console.WriteLine(" Average fitness: " + ave.ToString());
+            Console.WriteLine($" Difference: {best.Fitness - th} ");
+            Console.WriteLine(" Mutation Rate: " + mut.ToString());
+        }
+    }
+}
---
+++ Piscosour/Piscosour/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Laga.GeneticAlgorithm;
+
+namespace Piscosour
+{
+    class Program
+    {
+        private static int sizePop = 200;
+        private static string target = "pneumonoultramicroscopicsilicovolcanoconiosis"; //"piscosour";
+
+        private static Population<char> pop = new Population<char>(sizePop);
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            GenrChromosome genrChromosome = new GenrChromosome(target.Length);
+            for(int i = 0; i < sizePop; i++)
+            {
+                Chromosome<char> chr = new Chromosome<char>(genrChromosome.CharChromosome(97, 122).ToList());
+                pop.Add(chr);
+            }
+
+            int count = 0;
+            float mutRate = 0.0015f;
+            Chromosome<char> best;
+            do
+            {
+                Evaluation();
+
+                best = pop.Higher();
+                PrintData(best, count, mutRate, pop.FitnessAverage());
+
+                Population<char> nPop = NaturalSelection<char>.RouletteWheelNonPolinomicMin(pop, 60);
+                Population<char> cPop = Crossover<char>.RandomPointCrossover(nPop, sizePop);
+                pop = Mutation.CharMutation(cPop, mutRate, 97, 122);
+
+                count++;
+            } while (best.Fitness < 0.999);
+
+            Console.ReadLine();
+        }
+
+        //we evaluate the whole population at once.
+        private static void Evaluation()
+        {
+            int c = 0;
+            int le = target.Length;
+            for (int i = 0; i < sizePop; i++)
+            {
+                c = 0;
+                Chromosome<char> chromosome = pop.GetChromosome(i);
+
+                for (int j = 0; j < chromosome.Count; ++j)
+                {
+                    if (chromosome.GetDNA(j) == target[j])
+                        c++;
+                }
+                chromosome.Fitness = c / target.Length;
+            }
+        }
+        private static void PrintData(Chromosome<char> best, int genCount, float mut,  double ave)
+        {
+            Console.SetCursorPosition(0, 1);
+            Console.Write(" Best individual: ");
+            Console.WriteLine(best.Chr2Str(""));
+            Console.WriteLine(" ");
+            Console.WriteLine(" ////////////// GA data");
+            Console.WriteLine(" Generation: " + genCount.ToString());
+            Console.WriteLine(" Highest fitness: " + Math.Round(best.Fitness, 3).ToString());
+            Console.WriteLine(" Average fitness: " + Math.Round(ave, 3).ToString());
+            Console.WriteLine(" Mutation Rate: " + mut.ToString());
+        }
+    }
+}
