33d6385211668b8692337b4b05662fe1dbaa0011
cidelab <cidelab@gmail.com>
2020-03-01 19:04:54 +0000

    testing

---
+++ LagaUnity/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace LagaUnity
+{
+    public class Utils
+    {
+
+        public int c;
+
+        public void AddValues(int a, int b)
+        {
+            c = a + b;
+        }
+
+        public static int GenerateRandom(int min, int max)
+        {
+            System.Random rand = new System.Random();
+            return rand.Next(min, max);
+        }
+
+        public void RenameObject(GameObject gameObject, string name)
+        {
+            gameObject.name = name;
+        }
+    }
+}
